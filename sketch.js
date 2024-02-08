3acee994302d2f293f71b26fafe5c0e62ef60bd5
MikeyDamager <87273305+MikeyDamager@users.noreply.github.com>
2024-02-08 11:47:08 +0000

    .

--- sketch.js
+++ sketch.js
@@ -4,6 +4,18 @@
   / /| | / __ \/ __ \/ / / __ \/ _ \/ ___/ _ \/ __ `__ \/ / __ \
  / ___ |/ / / / /_/ / / / / / /  __/ /  /  __/ / / / / / / / / /
 /_/  |_/_/ /_/\____/_/ /_/ /_/\___/_/   \___/_/ /_/ /_/_/_/ /_/ 
+
+
+AnoTheremin is another theremin. It uses tone.js for the sounds and gyroscope and x/y data for controls. No midi yet, but i'll get to that one day. 
+Currently the sound is made from two oscillators that are running through their own signal paths for effects and they both get passed to the master track. 
+One lead osc is playing a lead tone, and a bass osc is playing the same notes tuned several octaves lower
+
+Its designed for mobile, naturally. Its intended to be held in one hand like a computer mouse or remote control, but it only uses the index finger for controls. 
+Forward and back tilt controls the pitch which is currently locked to two octaves of the A minor pentatonic scale (notes A, C, D, E, G, A, C etc).
+Left and right tilt currently controls the modulation speed of the filter on the bass oscillator to make it wub. 
+x and y position data from the touchscreen control a number of different effect parameters 
+(as of writing this sentence I can't remember what they are, but read on and i'll talk you through them.)
+
 */
 
 
@@ -30,14 +42,49 @@ LLLLLLLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEEEEEEEEE      TTTTTTTTTTT
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const baseFrequency = 220; 
+
+
+//chromatic
+
+const rootFrequency = 220; 
 const twelfthRootOf2 = Math.pow(2, 1 / 12);
-
 let chromatic = false
+/*
+To get us off to a good start, i dont actually currently use these first  variables, they will just be helpful later because its the formula for an equal tempered chromatic scale.
+I'm going to immediately get sidetracked here by talking about music theory, because thats what i do, 
+but this is a good formula to know if you're making music related things. 
 
+In order to counteract a whole bunch of issues caused by historical methods of tuning musical notes to simple, low integer ratios of eachother
+a number of major composers in the 18th century started using a tuning that evenly dispersed 12 notes across an octave and this is what most modern pianos are now tuned to.
+
+- Take a base frequency (the A above middle C (A4) on a piano is usually tuned to 440hz so it an easy one to remember).
+
+- multiply that note by the 12th root of 2 (or 1.05946309436) and you've got the frequency of the next key on the piano. 
+
+- multiply the second note by the same number and keep going until you end up with double the frequency you started with.
+
+-this is your octave, you can multiply or divide them by 2 to shift into higher or lower octaves respectively.
+
+
+unfortunately, the chromatic scale makes this instrument sound like a pissed off cat until we can implement a better system of triggering the sound and calibrate the tilt action a little better.
+*/
+
+
+
+//tone.js variables for the nodes in our signal path. 
 const volume = -15;
 let lead, bass, hiPass, loPass, wub, crusher, chorus, verb, vib, trem
-  
+
+let frequency
+let modSpeed
+let bits
+
+//fft
+let spec
+
+let audioStart = false
+
+//tilt data 
 let askButton;
 let frontToBack = 0; 
 let leftToRight = 0; 
@@ -46,19 +93,13 @@ let tiltPos = 0
 let twistPos = 0
 let scalePos = 0
 
-let frequency
+//font
+let stacker
 
-let modSpeed
+//gonna put a menu splash screen in
+let splash = true
 
-let bits
 
-let multi = 1
-
-let audioStart = false
-
-let meth, stacker, zyborg
-
-let myFFT, bgSound
 
 
 
@@ -87,22 +128,25 @@ LLLLLLLLLLLLLLLLLLLLLLLL     OOOOOOOOO          OOOOOOOOO     PPPPPPPPPP
 function preload(){
 
   stacker = loadFont('assets/stacker.ttf')
-  zyborg = loadFont('assets/zyborgs.otf')
 
-  myFFT = new Tone.Waveform();
+  spec = new Tone.Waveform();
 
 }
 
 function setup () {
   
   createCanvas(windowWidth, windowHeight);
-  background(50);
-  createSynth()
-  motionListeners()
-  strokeWeight(3)
   rectMode(CENTER)
   angleMode(DEGREES)
-  noFill();
+  background(50);
+  strokeWeight(3)
+
+
+//I put all the tone.js and U/I bits and bobs in their own functions.
+  createEffects()
+  createSynth()
+  motionListeners()
+ 
   stroke(255,20);
 
 }
@@ -119,28 +163,43 @@ function draw() {
  stroke(255,20)
   text("AnoTheremin", height/2,-width*0.9)
   pop()
-  
+
+
+  //foward back tilt is mapped to 11 notes that are currently set to two octaves of the A minor pentatonic scale
   scalePos = int(map(tiltPos, 0, height, 1, 11))
-  
-  modPos = map(twistPos,0,width,1,3)
-  
+
+  //left to right twist is mapped to the modulation speed on a lowpass filter to make the bass wub
   modFreq = map(twistPos,0,width,-5,5)
   
+  //note calculation happens in another function. 
   scaleLogic()
 
-  lead.setNote(frequency*2);
-  bass.setNote(frequency/4);
-  
-  let cutoffFreqX = map(mouseX, 0, width, 1000, 20000);
-  let cutoffFreqY = map(mouseY, 0, height, 2000, 20);
-  
-  let chorFreq = map(mouseY, 0, height, 1, 10)
-  
-  let bits = map(mouseY, 0, height, 0, 6);
-  let depth = map(mouseX, 0, width, 0, 0.5)
-  
-  let room = map(mouseX,0,width,0.2,0.7)
 
+  //update the oscillator frequencies
+  lead.setNote(frequency*2);//lead osc doubles the base frequency to sound one octave higher than the note we have calculated
+  bass.setNote(frequency/4);//bass osc divides the base frequency to sound two octaves lower. 
+
+  /*
+  These next variables map the user actions to different effect parameters. Effect routing is where the real fun happens. 
+  */
+
+  //FILTER MAPS - human hearing range is from around 20hz to 20Khz
+
+  //x mouse position maps to a range between 1000 and 200000 from left to right for the hipass filter.
+  let cutoffFreqX = map(mouseX, 0, width, 1000, 20000);
+  //y mouse maps from 2000 to 20 top to bottom for the lowpass filter on the bass
+  let cutoffFreqY = map(mouseY, 0, height, 2000, 20);
+
+
+  let chorFreq = map(mouseY, 0, height, 1, 10) //this is the speed of the chorus effect. 1 - 10 hz. 
+
+  let bits = map(mouseY, 0, height, 0, 6); //bit size for the bitcrusher distortion
+
+  let depth = map(mouseX, 0, width, 0, 0.5) //depth setting for the reverb. effectively how much wet signal is mixed in with the dry.
+  let room = map(mouseX,0,width,0.2,0.7) //the size of the reverberations.
+
+
+//these just link the mapped input data to their respective effect nodes. 
   hiPass.frequency.value = cutoffFreqX;
   loPass.frequency.value = cutoffFreqY;
   wub.frequency.value = modFreq
@@ -151,9 +210,9 @@ function draw() {
   chorus.frequency.value = chorFreq/2
   chorus.depth.value = depth/4
   
-    spectro()
-    tiltMap()
-    mouseLoc()
+  spectro()//draws the waveform
+  tiltMap()//maps the tilt position to 11 possible notes
+  mouseLoc()//draws the mouse location indicator.
 
 }
 
@@ -178,15 +237,22 @@ MMMMMMMM               MMMMMMMM     OOOOOOOOO           UUUUUUUUU       SSSSSSSS
                                                                                                                
 */
 function mousePressed () {
+<<<<<<< Updated upstream
   if (Tone.context.state !== 'running') {
     Tone.context.resume();
   }
     
+=======
+
+  //when mouse is pressed, it triggers the amplitude envelope for the oscillator and sustains until released.
+>>>>>>> Stashed changes
   lead.triggerAttack();
   bass.triggerAttack();
 }
 
 function mouseReleased () {
+
+  //release triggers the release envelope. 
   lead.triggerRelease();
   bass.triggerRelease();
 }
@@ -212,99 +278,117 @@ S:::::::::::::::SS    OO:::::::::OO       UU:::::::::UU    N::::::N        N::::
 
 function createSynth() {
   
+//Two FM synths are created, one triangle lead sound one square for bass. 
+//They both have portamento turned on, which is how the notes can glide between eachother which is key to emulating a theremin type sound. 
+//The lead takes longer to drift betwen notes than the bass.
+//I want to get a whole bank of synth patches that can be changed on the fly, this one is just a starting point, and its already annoying the fuck out of me having to listen to over and over again
 
-  //lead synth
+
+//FM synthesis can get very weird and complicated and unnecessary and quite frankly I hate it, but here it is anyway. 
+//the frequency of a carrier signal is modulated by another signal to create more complex harmonic activity from simple waveforms. 
+//feel free to change any node for a different one.
+
   lead = new Tone.FMSynth({
-    harmonicity: 3,
-    modulationIndex: 10,
-    detune: 0,
-    oscillator: {
-        type: "triangle"
+    harmonicity: 2, //this figures out the ratio of modulation signal vs the carrier. 1 is unison, 2 is an octave, 3 is octave+perfect 5th, etc.
+    //effectively, this is causing the frequency to quickly change between the root and an octave higher so the sound has a more complex timbre. 
+
+    modulationIndex: 10,//modulation signal depth
+
+    detune: 0, //modify base frequency by cents (100ths of a semitone) not hz.
+
+    oscillator: {type: "triangle"},//the shape of the carrier signal that is actually heard.
+    //amplitude envelope.
+    
+    
+    envelope: {//shapes the amplitude of the signal.
+
+      attack: 0.01, //attack is the time it takes for a sound to reach its loudest volume after triggering
+      decay: 0.01, //decay is how long after the peak it takes to fall to the sustain volume
+      sustain: 1, //sustain is the volume of note as it holds before releasing
+      release: 0.5 //release is how long it takes for the amplitude to return to zero when the note is released 
+
     },
-    envelope: {
-        attack: 0.01,
-        decay: 0.01,
-        sustain: 1,
-        release: 0.5
-    },
-    modulation: {
-        type: "square"
-    },
-    modulationEnvelope: {
-        attack: 0.5,
-        decay: 0,
-        sustain: 1,
-        release: 0.5
-    },
-    portamento: 0.1 // Adjust the value as needed
+
+
+    modulation: {type: "sine"},//shape of the modulation signal.
+    modulationEnvelope: {attack: 0.5, decay: 0, sustain: 1, release: 0.5},
+
+    portamento: 0.1 //the speed at which the oscillator drifts between frequencies when new notes are triggered.
 });
 
-let leadGain = new Tone.Gain(0.2)
 
+//same deal with the bass synth, but different settings. 
+bass = new Tone.FMSynth({
+
+  harmonicity: 3,
+
+  modulationIndex: 10,
+
+  detune: 0,
+
+  oscillator: {type: "sine"},
+
+  envelope: {attack: 0.01, decay: 0.01, sustain: 1, release: 0.5},
+
+  modulation: {type: "square"},
+
+  modulationEnvelope: {attack: 0.5,decay: 0,sustain: 1,release: 0.5},
+
+  portamento: 0.05 
+});
+
+
+//each oscillator gets a gain node to balance their respective volumes in the signal path. 
+let leadGain = new Tone.Gain(0.2)
+let bassGain = new Tone.Gain(0.4)
+
+
+//connect the oscs to the gain nodes. 
+  bass.connect(bassGain)
   lead.connect(leadGain)
   
-  bass = new Tone.FMSynth({
-    harmonicity: 3,
-    modulationIndex: 10,
-    detune: 0,
-    oscillator: {
-        type: "sine"
-    },
-    envelope: {
-        attack: 0.01,
-        decay: 0.01,
-        sustain: 1,
-        release: 0.5
-    },
-    modulation: {
-        type: "square"
-    },
-    modulationEnvelope: {
-        attack: 0.5,
-        decay: 0,
-        sustain: 1,
-        release: 0.5
-    },
-    portamento: 0.05 // Adjust the value as needed
-});
+//This is how the audio signal path connects. 
+//It's not too crazy at the moment but his is where things can get real confusing really quick without a robust naming system and real cables to visualise everything.
 
-let bassGain = new Tone.Gain(0.4)
-  bass.connect(bassGain)
   
+  leadGain.connect(vib); //Lead gain goes into vibrato, to give it the voicelike, wavering quality to the pitch. 
+  vib.connect(hiPass) //vibrato goes into hipass filter
+  hiPass.connect(verb) //hipass goes into the reverb
+  verb.connect(Tone.Master); //reverb connects to master output.
   
-  hiPass = new Tone.Filter();
-  loPass = new Tone.Filter();
-  wub = new Tone.AutoFilter({
-    frequency: 2, 
-    depth: 1 
-  }).start();
-  trem = new Tone.Tremolo()
-  vib = new Tone.Vibrato()
+  bassGain.connect(loPass); //bassgain goes to lopass filter.
+  loPass.connect(wub); //lopass goes into the second, modulated lowpass filter that controls the wubs. 
+  wub.connect(Tone.Master);//wub filter goes into master output. . 
   
-  crusher = new Tone.BitCrusher()
-  chorus = new Tone.Chorus()
-  verb = new Tone.JCReverb()
-  
-  leadGain.connect(vib);
-  vib.connect(hiPass)
-  
-  hiPass.connect(verb)
-  
-  bassGain.connect(loPass); 
-  
-  verb.connect(Tone.Master);
-  loPass.connect(wub);
-    
-  wub.connect(Tone.Master);
-  
-  Tone.Master.connect(myFFT)
+  Tone.Master.connect(spec); //finally, the effected master output is sent to the fft to draw the waveform. 
  
-  
 }
 
+function createEffects(){
+
+  //creates all the effects. 
+  hiPass = new Tone.Filter(); //hipass filter allows all frequencies over a certain number.
+  loPass = new Tone.Filter(); //lopass filter allows all frequencies under a certain number. 
+//this allows us to keep a degree of separation between the bass and the lead if the timbres get too harmonically complex.
+//It stops the basstone from producing frequencies that might clutter up the high end, and vice versa. 
+
+
+  wub = new Tone.AutoFilter({frequency: 2, depth: 1}).start();//autofilter is where the wubs come from. left and right tilt controls the speed of this.
+
+  trem = new Tone.Tremolo();//tremolo modulates signal volume.
+  vib = new Tone.Vibrato() //vibrato modulates signal pitch. 
+  crusher = new Tone.BitCrusher() //bitcrusher digitally downsamples the audio to different bitrates and creates that beautiful digital speak n spell type distortion that can also sound like absolute garbage if you arent careful (i love it)
+  chorus = new Tone.Chorus() //chorus is supposed to mimic slight tuning and timing discrepancies between two musicians playing something in unison. 
+  // It doubles (or more) the sound being played, and plays back the copies with very slight, almost imperceptible delays. 
+  //this has the effect of modulating the pitch we hear and thickening up the sound. It's a good way of making music sound very 80s. 
+  verb = new Tone.JCReverb()//reverb is reverb. its sound bouncing off walls. 
+}
+
+
+//draws the waveform from the fft
 function spectro() {
   if (mouseIsPressed) {
-      const waveform = myFFT.getValue();
+      const waveform = spec.getValue();
       const waveformLength = waveform.length;
       const step = width / waveformLength;
       beginShape();
@@ -343,29 +427,31 @@ S:::::::::::::::SS      CCC::::::::::::C A:::::A                 A:::::A L::::::
 */
 
 function scaleLogic(){
+
+  //if chromatic mode were true, it would calculate the notes this way, but its not so it doesnt. 
     if(chromatic){
       if (scalePos >= 1 && scalePos <= 15) {
-  frequency = baseFrequency * Math.pow(twelfthRootOf2, scalePos - 1);
+  frequency = rootFrequency * Math.pow(twelfthRootOf2, scalePos - 1);
 } else { 
         frequency = 0; 
 }
   }else{
   
-  
+  //manually assigns the notes in our scale
   if(scalePos==1){
-  frequency = 220
+  frequency = 220 // Root A
   }else if(scalePos==2){
-  frequency = 261.63
+  frequency = 261.63 //minor 3rd C
   } else if(scalePos==3){
-  frequency = 293.66
+  frequency = 293.66 //perfect 4th D
   } else if(scalePos==4){
-  frequency = 329.63
+  frequency = 329.63 //perfect 5th E
   } else if(scalePos==5){
-  frequency = 392
+  frequency = 392 //minor 7th G
   }else if(scalePos==6){
-  frequency = 440
+  frequency = 440 //octave
   }else if(scalePos==7){
-  frequency = 523.25
+  frequency = 523.25 //etc...
   }
   else if(scalePos==8){
   frequency = 587.33
@@ -374,23 +460,21 @@ function scaleLogic(){
   }else if(scalePos==10){
   frequency = 783.99
   } else if(scalePos==11){
-  frequency = 880
+  frequency = 880 //octave
   }
   
   }
 }
 
-/*                                                                                                    
-                                                                      
-*/
 
 function tiltMap(){
-  
+  //maps -45 to 135 degrees of forward/backwards tilt to display height
   tiltPos = constrain(map(frontToBack, -45,135,0,height),0,height)
-  
+  //maps -45 to 45 degrees of twist to display width
   twistPos = constrain(map(leftToRight, -45,45,0,width),0,width)
  ;
  
+ //draws the keys
   noFill()
    for(let i = 0; i<11; i++){
     push()
@@ -400,11 +484,15 @@ function tiltMap(){
   }
   textSize(15);
   fill(255,50)
+
+  //indicator for the tilt/twist
   circle(twistPos,tiltPos,50)
  
 }
 
 function mouseLoc(){
+
+  //draws an indicator at mouse location. the opacity fade is all fucked up and i havent fixed it yet. 
   const dim = Math.min(width, height);
    if(mouseIsPressed){
      opacity = 0
@@ -447,7 +535,7 @@ function handlePermissionButtonPressed(){
     DeviceOrientationEvent.requestPermission()
     .then(response => {
       if (response === 'granted') {
-        // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+       
         window.addEventListener('deviceorientation', deviceTurnedHandler, true)
       }
     })
@@ -460,24 +548,24 @@ function deviceMotionHandler(event){
   accY = event.acceleration.y;
   accZ = event.acceleration.z;
   
-  rrateZ = event.rotationRate.alpha;//alpha: rotation around z-axis
-  rrateX = event.rotationRate.beta;//rotating about its X axis; that is, front to back
-  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
+  rrateZ = event.rotationRate.alpha;
+  rrateX = event.rotationRate.beta;
+  rrateY = event.rotationRate.gamma;
   
 }
 
 function deviceTurnedHandler(event){
   
-  frontToBack = event.beta; // beta: front back motion
-  leftToRight = event.gamma; // gamma: left to right
+  frontToBack = event.beta; 
+  leftToRight = event.gamma; 
 
 }
 
 function motionListeners(){
     if(typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function'){
   
-    askButton = createButton('Permission');//p5 create button
-    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
+    askButton = createButton('Permission');
+    askButton.mousePressed(handlePermissionButtonPressed);
   }else{
     
     window.addEventListener('devicemotion', deviceMotionHandler, true);
